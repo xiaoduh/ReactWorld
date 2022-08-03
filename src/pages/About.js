32f3678a5b94d8894e87270b4dcbbe799a3fed9e
Clément <83875368+xiaoduh@users.noreply.github.com>
2022-08-03 12:10:35 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ipsum dolorum, dolorem facilis temporibus sint ea at tempore. Deserunt beatae laudantium incidunt expedita distinctio culpa at officia, repellat commodi voluptate enim odit adipisci fugit sunt rem similique ullam doloribus quaerat labore harum earum sint, est fugiat. Nisi odio possimus minus tempore ducimus ut nesciunt sapiente delectus. Dignissimos omnis ipsum facilis eveniet aspernatur nihil itaque est provident. Facilis beatae quae doloribus enim adipisci, quaerat ducimus similique quasi. Tenetur animi tempore perferendis voluptas nam cum, rem aperiam alias esse tempora beatae nemo, nihil ipsa ea! Adipisci laborum unde minus ea, consectetur dicta?</p>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A earum beatae obcaecati ratione nemo aspernatur, eum iste temporibus? Cumque quo hic soluta pariatur facere sapiente minima fugiat delectus? Iste eligendi harum fuga similique atque itaque odio adipisci doloribus! Laboriosam perferendis itaque labore et, possimus dolor officia corrupti doloremque? Sit assumenda autem distinctio, veniam possimus veritatis? Doloremque officiis culpa iure architecto commodi nemo odio vero sit quod dolor, maiores voluptatibus sint numquam vel magnam illum omnis, repellendus accusantium sapiente aliquid repudiandae sunt? Aspernatur, labore excepturi dolore laborum dicta praesentium placeat mollitia fugit, nemo, ad esse vero ab deleniti minima hic aliquid?</p>
+        </div>
+    );
+};
+
+export default About;
